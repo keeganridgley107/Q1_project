d7f60a24acec2f89fa2a6fd80f21294af7f92783
keegan ridgley <keegan.cabin@gmail.com>
2016-10-28 17:05:20 -0600

    added button for image change

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 "use strict";
+$( document ).ready(function() {
+  //function for google custom search is from google api docs
+  //jquery is having trouble loading??
 
   (function() {
     var cx = '010106870863249008865:hfaciltwsa8';
@@ -8,4 +11,17 @@
     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(gcse, s);
-  })();
+})();
+
+
+
+    console.log( "ready!" );
+
+
+    function changeImage() {
+      console.log("iwasclicked");
+
+    }
+    $("#image_button").on("click", changeImage);
+
+});
