a72707dd6bfb18cde5a43d100ad4c2b5491d95ac
keegan ridgley <keegan.cabin@gmail.com>
2016-10-28 14:34:08 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+  (function() {
+    var cx = '010106870863249008865:hfaciltwsa8';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
