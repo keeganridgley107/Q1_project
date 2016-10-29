46441da25075eceb891d535cb449d1b288de6272
keegan ridgley <keegan.cabin@gmail.com>
2016-10-29 13:05:01 -0600

    rm the text send func

--- app.js
+++ app.js
@@ -21,13 +21,6 @@ $( document ).ready(function() {
     function changeImage() {
       console.log("iwasclicked");
 
-      var text = require('textbelt');
-
-text.send('5038036811', 'A sample text message!', undefined, function(err) {
-  if (err) {
-    console.log(err);
-  }
-});
     }
     $("#image_button").on("click", changeImage);
 
