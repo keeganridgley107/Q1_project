c782b77c1b630c889dfa412017f384d238f0dd53
keegan ridgley <keegan.cabin@gmail.com>
2016-10-29 13:03:52 -0600

    fixed align and results page

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ $( document ).ready(function() {
     function changeImage() {
       console.log("iwasclicked");
 
+      var text = require('textbelt');
+
+text.send('5038036811', 'A sample text message!', undefined, function(err) {
+  if (err) {
+    console.log(err);
+  }
+});
     }
     $("#image_button").on("click", changeImage);
 
