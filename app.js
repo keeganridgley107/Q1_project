48157b9160b7112efd355cfb2f686d4d2a1510e6
keegan ridgley <keegan.cabin@gmail.com>
2016-10-29 18:25:03 -0600

    moved logo and remade header

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ $( document ).ready(function() {
 
     console.log( "ready!" );
 
+    var images = ['https://unsplash.com/collections/325867/minimalism?photo=JdUzpKdBsbI','https://unsplash.com/collections/325867/minimalism?photo=Bfrk9RCOhRE','https://unsplash.com/collections/325867/minimalism?photo=-S6R604fv_c','https://unsplash.com/collections/325867/minimalism?photo=HeFNnUaDiWc','https://unsplash.com/collections/325867/minimalism?photo=1fyccRaS_u4'];
+
+
+
+
+    $('#imageBannerDiv')
 
     function changeImage() {
       console.log("iwasclicked");
